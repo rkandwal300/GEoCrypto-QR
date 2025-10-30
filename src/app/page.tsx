30899baae71895b25bfe39e4d17a2215d2924455
Rahul Kandwal <rahulkandwal707@gmail.com>
2025-10-30 15:38:59 +0000

    Try fixing this error: `Runtime Error: Error: Element type is invalid: e

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { QrGenerator } from "@/components/qr-generator";
-import ChatWidget from "@/components/chat/ChatWidget";
+import { ChatWidget } from "@/components/chat/ChatWidget";
 
 export default function GeneratePage() {
-  return <ChatWidget />;
+  return <ChatWidget userId="user1" otherId="user2" title="Support Chat" />;
   // return <QrGenerator />;
 }
