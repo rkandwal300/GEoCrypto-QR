15b94ea664a475ba05a206b0a0166dce6693fd84
Rahul Kandwal <rahulkandwal707@gmail.com>
2025-11-01 12:40:37 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/lib/AntdRegistry.tsx
+++ src/lib/AntdRegistry.tsx
@@ -1,24 +1,5 @@
 "use client";
 
-import React from "react";
-import { createCache, extractStyle, StyleProvider } from "@ant-design/cssinjs";
-import { useServerInsertedHTML } from "next/navigation";
 import { AntdRegistry as NextAntdRegistry } from '@ant-design/nextjs-registry';
-
-/**
- * @deprecated Use `AntdRegistry` from `@ant-design/nextjs-registry` instead.
- * This custom registry is kept for reference but the official one is preferred.
- */
-export const CustomAntdRegistry = ({ children }: { children: React.ReactNode }) => {
-  const cache = createCache();
-  useServerInsertedHTML(() => (
-    <style
-      id="antd"
-      dangerouslySetInnerHTML={{ __html: extractStyle(cache, true) }}
-    />
-  ));
-  return <StyleProvider cache={cache}>{children}</StyleProvider>;
-};
-
 
 export const AntdRegistry = NextAntdRegistry;
